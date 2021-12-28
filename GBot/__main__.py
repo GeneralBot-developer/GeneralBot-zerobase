45e3594fd5aa9287b932002863c013b0dfd8498e
kousakirai <kousakiraiminecraft@yahoo.co.jp>
2021-12-29 00:29:08 +0900

    update: jishaku有効化

--- GBot/__main__.py
+++ GBot/__main__.py
@@ -1,4 +1,4 @@
 from GBot.core import GeneralBotCore
 import os
 
-GeneralBotCore(prefix="!", token=os.environ["BOT_TOKEN"], jishaku=False).run()
+GeneralBotCore(prefix="!", token=os.environ["BOT_TOKEN"], jishaku=True).run()
