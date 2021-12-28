26351f9e39c58bfa99693a8d8c7953899b3d65a3
kousakirai <kousakiraiminecraft@yahoo.co.jp>
2021-12-29 00:36:25 +0900

    fix: Jishakuインストールと再起動用コマンド追加

--- GBot/cogs/Utils.py
+++ GBot/cogs/Utils.py
@@ -1,4 +1,6 @@
-from nextcord.ext.commands import Cog, command
+from nextcord.ext.commands import Cog, command, is_owner, group
+import os
+import sys
 
 
 class BotUtility(Cog):
@@ -9,6 +11,23 @@ class BotUtility(Cog):
     async def ping(self, ctx):
         async with ctx.typing():
             await ctx.reply(f"Pong! {round(self.bot.latency * 1000)}ms.")
+
+    @is_owner()
+    @group()
+    async def moding(self, ctx):
+        if ctx.invoked_subcommand is None:
+            return
+
+    @moding.command()
+    @is_owner()
+    async def reboot(self, ctx):
+        await ctx.reply("rebooting...")
+        python = sys.executable
+        os.execl(
+            python,
+            python,
+            *sys.argv
+        )
 
     def setup(bot):
         bot.add_cog(BotUtility(bot))
