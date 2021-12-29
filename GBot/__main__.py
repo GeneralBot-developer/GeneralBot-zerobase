6760745d38895120a7c1a9952e2662400d87005a
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2021-12-29 11:01:15 +0900

    owner_ids作成
    ほぼ適当だよ
    丸投げされたんで

--- GBot/__main__.py
+++ GBot/__main__.py
@@ -1,4 +1,6 @@
 from GBot.core import GeneralBotCore
 import os
 
-GeneralBotCore(prefix="!", token=os.environ["BOT_TOKEN"], jishaku=True).run()
+bot = GeneralBotCore(prefix="!", token=os.environ["BOT_TOKEN"], jishaku=True)
+bot.owner_ids = [484655503675228171, 693025129806037003, 898929156048441386]
+bot.run()
